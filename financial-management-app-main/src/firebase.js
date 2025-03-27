ceda88432841401f891608c662a07e93eb8c5959
Aishwarya Sanjay Sathe <108823419+Aishu862@users.noreply.github.com>
2025-03-27 17:41:03 +0530

    Initial commit

---
+++ financial-management-app-main/src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const app = initializeApp({
+  
+  apiKey: "AIzaSyAOB9l2bh9dTD0MRxi0A9QOZWckIPQFuoE",
+  authDomain: "saferoute-407318.firebaseapp.com",
+  databaseURL: "https://saferoute-407318-default-rtdb.firebaseio.com",
+  projectId: "saferoute-407318",
+  storageBucket: "saferoute-407318.appspot.com",
+  messagingSenderId: "167829110927",
+  appId: "1:167829110927:web:f2d860dbb885bf6d0bb0fa",
+  measurementId: "G-TSZ3X00PWY"
+});
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
